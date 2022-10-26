d5514cad5b2a8873df6556a56d16fd7e9225fa89
JoaoVictorG <joaovictorgon36@gmail.com>
2022-10-26 10:53:30 -0300

    Create tables Cliente and Imovel

--- Comandos.sql
+++ Comandos.sql
@@ -1,2 +1,32 @@
 /*    CREATE DATABASE    */
 CREATE Database [dbTeste]
+
+Drop TABLE [Cliente]
+
+/*    CREATE TABLE CLIENTE    */
+CREATE TABLE [Cliente] (
+    [ClienteId] INT NOT NULL,
+    [Cliente] NVARCHAR(160) NOT NULL,
+    [TipoCliente] NVARCHAR(160) NOT NULL,
+    [NomeContato] NVARCHAR(160) NOT NULL,
+    [TelefoneContato] BIGINT NOT NULL,
+    [Cidade] NVARCHAR(160) NOT NULL,
+    [Bairro] NVARCHAR(160) NOT NULL,
+    [Logradouro] NVARCHAR(160) NOT NULL,
+    [DataCadastro] DATETIME NULL,
+    [DataAtualizacao] DATETIME NULL
+
+    PRIMARY KEY([ClienteId])
+)
+
+CREATE TABLE [Imovel](
+    [ImovelId] INT NOT NULL,
+    [ClienteId] INT NOT NULL,
+    [Finalidade] NVARCHAR(160) NOT NULL,
+    [Valor] DECIMAL(18,2) NOT NULL,
+    [DataCadastro] DATETIME NULL
+
+    PRIMARY KEY([ImovelId])
+    CONSTRAINT [FK_Cliente_Imovel] FOREIGN KEY([ClienteId])
+        REFERENCES [Cliente](ClienteId)
+)
